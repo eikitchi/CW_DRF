772099ef717d055607a5948fe4810bdc6e2fcf6e
German <germanstarinenko@gmail.com>
2023-08-28 09:52:48 +0300

    Добавление тестов +-

--- main/users/views.py
+++ main/users/views.py
@@ -1,5 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
 from main.users.models import User
 from main.users.permissions import IsOwner
@@ -10,6 +11,13 @@ class UserCreateAPIView(generics.CreateAPIView):
     """Контроллер для создания пользователя"""
 
     serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserListAPIView(generics.ListAPIView):
